00146892135108761d8d3af5cd387e992fb591ae
sky_wu <sky.wu.gss@gmail.com>
2017-01-05 17:38:46 +0800

    bug-fix *employment services preparation *version information *function script

--- 1-source/1-code/4.employment/employmentServices.cs
+++ 1-source/1-code/4.employment/employmentServices.cs
@@ -1,3 +1,6 @@
 #employment services
 #檢查是否為以前任職員工？
 getEmployment(party_id, position_id)
+{
+  bug-fixed on release 1.0 (pre)
+}
---
+++ 1-source/4-data/authority.sql
@@ -0,0 +1,2 @@
+insert into functions(fun_id, fun_name) values('hr0201', '招募作業')
+insert into functions(fun_id, fun_name) values('hr0301', '任用作業')
