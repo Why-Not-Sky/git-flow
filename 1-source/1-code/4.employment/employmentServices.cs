83a2f58524ab69a4cfa2bb58e15da8f935ac56ea
sky_wu <sky.wu.gss@gmail.com>
2017-01-18 16:07:11 +0800

    bug *[1.0.2-2017/01/18 sky-on-air-2016@gss add some valdiation] *[1.0.2-2017/01/18 sky-on-air-2016@gss add HealthInsurance Code]

--- 1-source/1-code/4.employment/employmentServices.cs
+++ 1-source/1-code/4.employment/employmentServices.cs
@@ -4,9 +4,12 @@ getEmployment(party_id, position_id)
 {
   bug-fixed on release 1.0 (pre)
   //[1.0.1-2017/01/06 toshiba@gss fixed 未判斷聘用狀況、在職離職、留停]
-
+  //[1.0.2-2017/01/18 sky-on-air-2016@gss add some valdiation]
+  status = 1
 
   //[1.0.1-2017/01/06 sky-on-air-2016@gss fixed 有多次進出時的錯誤]
-  lastRecord = exec_sql('select max() from employment')
+  //lastRecord = exec_sql('select max() from employment')
+  //[1.0.2-2017/01/18 sky-on-air-2016@gss add some valdiation]
+  lastRecord = exec_sql('select max() from employment where status =1')
 
 }
